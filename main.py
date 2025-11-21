f5ab46e5f4535cf68292cb59823de1892443a9d7
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2025-11-21 07:05:42 +0100

    Add test for subtraction

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
 def addition(a, b):
     return a + b
-    
+
+def subtraction(a, b):
+    return a - b
 print("Hello, World!")
 print("This is an update!")
 # My name is Q
--- test.py
+++ test.py
@@ -10,5 +10,8 @@ class TestMain(unittest.TestCase):
         self.assertEqual(main.addition(40, 20), 60, "Should be 60")
         self.assertEqual(main.addition(-3, 2), -1, "Should be -1")
 
+    def test_subtraction(self):
+        self.assertEqual(main.subtraction(5, 4), 1, "Should be 1")
+
 if __name__ == "__main__":
     unittest.main()
