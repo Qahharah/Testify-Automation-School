d1f639f1265a2ffacca5ead24b305268cff85492
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2025-11-27 13:29:43 +0100

    Added task 6 solution

---
+++ Module4/4A/task6.js
@@ -0,0 +1,8 @@
+// Step 1: Initialize the variable
+let number = 17;
+// Step 2: Check if the number is even or odd
+if (number % 2 === 0) {
+    console.log(number + " is even");
+} else {
+    console.log(number + " is odd");
+}
