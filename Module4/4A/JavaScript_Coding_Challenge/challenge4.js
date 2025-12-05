78967e91ea346edc79d606b322466a7ac7cc0bd4
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2025-12-05 18:37:19 +0100

    Added challenge 4 solution

---
+++ Module4/4A/JavaScript_Coding_Challenge/challenge4.js
@@ -0,0 +1,11 @@
+// Challenge 4: Full multiplication table from 1 to 12
+
+for (let i = 1; i <= 12; i++) {
+    console.log(`Multiplication Table for ${i}`);
+
+    for (let j = 1; j <= 12; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+    }
+
+    console.log("-----------------------------");
+}
