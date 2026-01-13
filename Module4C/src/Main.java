074757f94bade5bbc71f714de4c0e6697e204933
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2026-01-13 15:41:06 +0100

    Task 1: print output to console

---
+++ Module4C/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task 1: Java is working");
+        System.out.println("This is Module 4C");
+        System.out.println("Console output successful");
+    }
+}
