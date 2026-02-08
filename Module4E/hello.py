d980447031910fbd7a8bf21abe6521fc89f8d424
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2026-02-08 23:43:01 +0100

    Add Hello Appium

---
+++ Module4E/hello.py
@@ -0,0 +1,6 @@
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+print ("Hello Appium")
